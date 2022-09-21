da48656742a11082a18c4be037e4b8e6789dcf66
fejbien <sucholasfejbien@gmail.com>
2022-09-21 12:48:39 +0200

    cos

---
+++ memes.sql
@@ -0,0 +1,7 @@
+SELECT * FROM memes WHERE deleted_at IS NULL;
+
+INSERT INTO users(user_name, password, email, is_admin)
+VALUES ("Fabianooo", "4d00d79b6733c9cc066584a02ed03410", "fabian.sucholas123@gmail.com", true)
+
+INSERT INTO users(user_name, password, email, is_admin)
+VALUES ("Nizeroo22o", "9d4f597a29f2a6d193fe94a53734c0ae", "nizero22opoaniejanie@gmail.com", false)
