6bfd921a2916e74ad6033ae1776da5f5ba67cc7b
Fabian <38659818+Fejbien@users.noreply.github.com>
2022-09-29 14:48:25 +0200

    Add files via upload

--- memes.sql
+++ memes.sql
@@ -6,7 +6,22 @@ VALUES ("Fabianooo", "4d00d79b6733c9cc066584a02ed03410", "fabian.sucholas123@gma
 INSERT INTO users(user_name, password, email, is_admin)
 VALUES ("Nizeroo22o", "9d4f597a29f2a6d193fe94a53734c0ae", "nizero22opoaniejanie@gmail.com", false);
 
-SELECT m.titile, m.file, m.likes, m.created_at as "date", u1.name as "author", u2.name as "admin" FROM memes "m"
-JOIN users "u1" ON m.user_id = u1.id
-JOIN users "u2" ON m.admin_id = u2.id
-WHERE m.deleted_at IS NULL OFFSET 0 LIMIT 10
+SELECT
+    m.title,
+    m.file AS "path",
+    m.likes,
+    m.created_at AS "date",
+    u1.user_name AS "author",
+    u2.user_name AS "admin"
+FROM
+    memes AS m
+JOIN users AS u1 ON
+    m.user_id = u1.id
+JOIN users AS u2 ON
+    m.admin_id = u2.id
+WHERE
+    m.deleted_at IS NULL AND 0 LIMIT 10;
+
+SELECT m.title, m.file AS "path", m.likes, m.created_at AS "date", u1.user_name AS "author", u2.user_name AS "admin" FROM memes AS m JOIN users AS u1 ON m.user_id = u1.id JOIN users AS u2 ON m.admin_id = u2.id WHERE m.deleted_at IS NULL AND 0 LIMIT 10
+
+SELECT u.user_name, c.content, c.created_at, c.modified_at, c.deleted_at FROM `comments` AS c JOIN users AS u ON c.user_id = u.id WHERE c.meme_id = 2;
